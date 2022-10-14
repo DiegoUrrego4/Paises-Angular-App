e776c24e2a8eac7dd3d8b75fc60f7f0ca4fec8b7
Diego Urrego <diegourrg@gmail.com>
2022-10-14 11:52:01 -0500

    Separación de componentes. Se inicia con la construcción del componente por país.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
@@ -7,6 +9,6 @@ import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [SidebarComponent, MenuComponent, FooterComponent],
   exports: [SidebarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
 })
 export class SharedModule {}
