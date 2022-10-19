342892926a51e5773418b3cbcc1c6b3dfd2daa59
Diego Urrego <diegourrg@gmail.com>
2022-10-19 13:52:00 -0500

    Cambios varios por cambio de servidor de desarrollo.

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class VerPaisComponent implements OnInit {
-
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(({ id }) => {
+      console.log(id);
+    });
   }
-
 }
